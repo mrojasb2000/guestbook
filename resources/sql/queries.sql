5c2ff8a837b36aad86be7c67dbffc2c680eddbab
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-22 21:41:54 -0300

    feat: added unit test guestbook

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,10 +1,10 @@
--- :name save-mesage! :! :n
+-- :name save-message! :! :n
 -- :doc creates a new messages using the name and message keys
 INSERT INTO guestbook
 (name, message)
 VALUES (:name, :message)
 
--- :name get-message :? :*
+-- :name get-messages :? :*
 -- :doc selects all available messages
 SELECT * FROM guestbook
 
